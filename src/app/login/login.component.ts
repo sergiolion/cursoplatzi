2377e0f384c9083c2b0ce858e58b54f851cd9434
sergiolion <slionel87@gmail.com>
2019-07-26 08:36:33 +0200

    2 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  operation: string = 'login';
+  email: string = null;
+  password: string = null;
+
+  constructor(private authenticacionService: AuthenticationService) { }
 
   ngOnInit() {
   }
 
+  login() {
+    this.authenticacionService.loginWithEmail(this.email, this.password)
+    .then(
+      data => {
+        alert('Loggeado correctamente');
+        console.log(data);
+      }
+    )
+    .catch( error => {
+      alert('Ha ocurrido un error');
+      console.log(error)
+    });
+  }
+
+  register() {
+    this.authenticacionService.registerWithEmail(this.email, this.password)
+    .then(
+      data => {
+        alert('Registrado correctamente');
+        console.log(data);
+      }
+    )
+    .catch( error => {
+      alert('Ha ocurrido un error');
+      console.log(error)
+    });
+  }
+  
 }
